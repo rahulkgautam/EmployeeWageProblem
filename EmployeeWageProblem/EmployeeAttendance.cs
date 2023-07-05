e0609d7a406ab5c36f04e0b7efdebf68c8d1e401
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-05 22:49:51 +0530

    Employee Attendance Program

---
+++ EmployeeWageProblem/EmployeeAttendance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeAttendance
+    {
+        public static  void CheckEmployeeAttendance()
+        {
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
