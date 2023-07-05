5f2d618cf1584163e50549d1cd85321b6b8521d2
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-05 23:14:30 +0530

    Calaculate Employee Daily Wage with part Program

--- EmployeeWageProblem/EmployeeDailyWage.cs
+++ EmployeeWageProblem/EmployeeDailyWage.cs
@@ -10,18 +10,17 @@ namespace EmployeeWageProblem
         {
             int EMP_RAT_PER_HOUR = 20;
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int empHour = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
             if (empCheck == IS_FULL_TIME)
-            {
                 empHour = 8;
-            }
+            else if (empCheck == IS_PART_TIME)
+                empHour = 4;
             else
-            {
                 empHour = 0;
-            }
             empWage = empHour * EMP_RAT_PER_HOUR;
             Console.WriteLine("Employee Wage :" + empWage);
         }
