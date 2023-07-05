854562ffcee4e64d9bba68840bba1dab3abc00f1
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-05 23:31:41 +0530

    Calaculate Employee Daily Wage A month of 20 days

--- EmployeeWageProblem/EmployeeDailyWage.cs
+++ EmployeeWageProblem/EmployeeDailyWage.cs
@@ -10,26 +10,33 @@ namespace EmployeeWageProblem
         public const int EMP_RAT_PER_HOUR = 20;
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
+        public const int NUM_OF_WORKING_DAY = 20;
         public static void CalculateEmployeeDailyWage()
         {
             int empHour = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
             {
-                case IS_FULL_TIME:
-                    empHour = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHour = 4;
-                    break;
-                default:
-                    empHour = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHour = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHour = 4;
+                        break;
+                    default:
+                        empHour = 0;
+                        break;
+                }
+                empWage = empHour * EMP_RAT_PER_HOUR;
+                Console.WriteLine("Employee Wage :" + empWage);
+                totalEmpWage += empWage;
             }
-            empWage = empHour * EMP_RAT_PER_HOUR;
-            Console.WriteLine("Employee Wage :" + empWage);
+            Console.WriteLine("Total EmpWage :" + totalEmpWage);
         }
     }
 }
